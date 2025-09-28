6dc28573b6d0a9ee01f6117baa446fcc79ebfe6a
pockyee <baoqi85@gmail.com>
2025-09-28 10:41:56 +0000

    bugfix

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ btn.addEventListener('click',()=>{
     let html = ''
     fetch(`https://www.thecolorapi.com/scheme?hex=${color.value.slice(1)}&mode=${mode.value}`)
         .then(res=>res.json())
-        .then(data=>{console.log(data.colors)
+        .then(data=>{
             for (let color of data.colors){
-                console.log(color.hex.value)
                 html += `
                 <div class="colorSet">
                     <div data-hex="${color.hex.value}" class="color" style="background-color: ${color.hex.value};"></div>
